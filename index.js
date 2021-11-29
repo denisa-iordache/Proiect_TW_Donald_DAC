5f35a28190597e269451ba549efee8c646c4fd44
denisa-iordache <74931542+denisa-iordache@users.noreply.github.com>
2021-11-29 19:16:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const statusRouter=require("./routes/status");
 
 const app = express();
 app.use("/status",statusRouter)
-app.set("port", process.env.PORT || 7000);
+app.set("port", 7000);
 
 app.listen(app.get("port"), () => {
   console.log(`Serverul a pornit pe http://localhost:${app.get("port")}`);
