fbd6173e93c0163e374d987d27b7c2a1fc3a1888
ivan-cristi <81228221+ivan-cristi@users.noreply.github.com>
2022-01-11 17:54:15 +0200

    Added the greeting & scrollable table to the Homepage

--- react_app/gui/src/components/HomepageComponent.js
+++ react_app/gui/src/components/HomepageComponent.js
@@ -1,9 +1,47 @@
 import React from "react";
+import '../css/Homepage.css'
 
 function Home() {
-    return(
-        <div>Home!!</div>
-    )
+  const user = "iFindAllBugs007";
+
+  return (
+    <div className="homepage">
+      <div>Welcome, dear {user}!</div>
+      <section>
+        <h1>Proiectele in care sunteti implicat</h1>
+        <div class="tbl-header">
+          <table cellpadding="0" cellspacing="0" border="0">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Nume Proiect</th>
+                <th>Status</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+        <div class="tbl-content">
+          <table cellpadding="0" cellspacing="0" border="0">
+            <tbody>
+              <tr>
+                <td>01</td>
+                <td>Tehnologii Web</td>
+                <td>In Progress</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>02</td>
+                <td>Multimedia</td>
+                <td>Done</td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
